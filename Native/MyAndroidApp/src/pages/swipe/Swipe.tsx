0b3a8b46524a42e772bf31c44eff9520f59ddb83
Голоцван Нікіта Дмитрович <Golo_bs09>
2026-03-28 14:52:34 +0200

    swipe update

--- Native/MyAndroidApp/src/pages/swipe/Swipe.tsx
+++ Native/MyAndroidApp/src/pages/swipe/Swipe.tsx
@@ -1,4 +1,4 @@
-import { Text, Touchable, useWindowDimensions, View } from "react-native";
+import { Text, TextBase, Touchable, useWindowDimensions, View } from "react-native";
 import SwipeStyle from "./ui/SwipeStyle";
 import { TouchableWithoutFeedback } from "react-native";
 import { GestureResponderEvent } from "react-native";
@@ -12,6 +12,8 @@ export default function Swipe()
     const shortestSide = Math.min(width, height);
     const fieldSide = 0.95 * shortestSide;
     const [text, setText] = useState<string>("");
+    const length = 16;
+    const [field, setField] = useState<Array<number>>(Array.from({length}, (_, i) => (3*i + 5) % 16));
 
     const onGestureBegin = (event: GestureResponderEvent) =>
     {
@@ -27,15 +29,136 @@ export default function Swipe()
     {
         if (eventBegin)
         {
-            setText(`dX=${event.nativeEvent.locationX - eventBegin.nativeEvent.locationX};
-                 dY=${event.nativeEvent.locationY - eventBegin.nativeEvent.locationY};`);
+            const dx = event.nativeEvent.pageX - eventBegin.nativeEvent.pageX;
+            const dy = event.nativeEvent.pageY - eventBegin.nativeEvent.pageY;
+            const dt = event.nativeEvent.timestamp - eventBegin.nativeEvent.timestamp
+
+            const lenX = Math.abs(dx);
+            const lenY = Math.abs(dy);
+            let result = "";
+
+            if (lenX > 2 * lenY)
+            {
+                if (lenX < 50 || dt < 200)
+                {
+                    result = "Horizontal: to short";
+                }
+                else if (lenX * 1.5 < dt)
+                {
+                    result = "Horizontal: too slow"
+                }
+                else if (dx > 0)
+                {
+                    result = "Horizontal: right";
+                    onSwipeRight();
+                }
+                else if (dx < 0)
+                {
+                    result = "Horizontal: left";
+                    onSwipeLeft();
+                }
+            }
+            else if (lenY > 2 * lenX)
+            {
+                if (lenY < 100 || dt < 200)
+                {
+                    result = "Vertical: to short";
+                }
+                else if (lenY * 2 < dt)
+                {
+                    result = "Vertical: too slow";
+                }
+                else if (dy < 0)
+                {
+                    result = "Vertical: top";
+                    onSwipeTop();
+                }
+                else if (dy > 0)
+                {
+                    result = "Vertical: bottom";
+                    onSwipeBottom();
+                }
+            }
+            else
+            {
+                result = "Diagonal";
+            }
+
+            // setText(`\ndX=${dx}; \ndY=${dy}; \ndt=${dt}; \nresult=${result}`);   
         }
     };
 
-    return <View style={{flexDirection: width < height ? "column": "row"}}>
+    const onSwipeRight = () =>
+    {
+        let emptyTileIndex = field.findIndex(i => i == 0);
+
+        if (emptyTileIndex % 4 == 0)
+        {
+            setText("Move disable");
+            return;
+        }
+
+        field[emptyTileIndex] = field[emptyTileIndex - 1];
+        field[emptyTileIndex - 1] = 0;
+        setField([...field]);
+    };
+
+    const onSwipeLeft = () =>
+    {
+        let emptyTileIndex = field.findIndex(i => i == 0);
+
+        if (emptyTileIndex % 4 == 3)
+        {
+            setText("Move disable");
+            return;
+        }
+
+        field[emptyTileIndex] = field[emptyTileIndex + 1];
+        field[emptyTileIndex + 1] = 0;
+        setField([...field]);
+    };
+
+    const onSwipeTop = () =>
+    {
+        let emptyTileIndex = field.findIndex(i => i == 0);
+
+        if (emptyTileIndex == 12 || emptyTileIndex == 13 || emptyTileIndex == 14 || emptyTileIndex == 15 )
+        {
+            setText("Move disable");
+            return;
+        }
+
+        field[emptyTileIndex] = field[emptyTileIndex + 4];
+        field[emptyTileIndex + 4] = 0;
+        setField([...field]);
+    }
+
+    const onSwipeBottom = () =>
+    {
+        let emptyTileIndex = field.findIndex(i => i == 0);
+
+        if (emptyTileIndex == 0 || emptyTileIndex == 1 || emptyTileIndex == 2 || emptyTileIndex == 3 )
+        {
+            setText("Move disable");
+            return;
+        }
+
+        field[emptyTileIndex] = field[emptyTileIndex - 4];
+        field[emptyTileIndex - 4] = 0;
+        setField([...field]);
+    }
+
+    return <View style={[SwipeStyle.pageContainer, {flexDirection: width < height ? "column": "row"}]}>
         <Text>Swipe: {text}</Text>
         <TouchableWithoutFeedback onPressIn={onGestureBegin} onPressOut={onGestureEnd}>
-            <View style={{width: fieldSide, height: fieldSide, backgroundColor: "#555"}}></View>
+            <View style={[SwipeStyle.gameField, {width: fieldSide, height: fieldSide}]}>
+                {field.map(i => 
+                <View style={SwipeStyle.tileContainer}>
+                    { i != 0 && <View style={SwipeStyle.tile}>
+                        <Text style={SwipeStyle.textBlock}>{i}</Text>
+                    </View>}
+                </View>)}
+            </View>
         </TouchableWithoutFeedback>
     </View>;
 }
--- Native/MyAndroidApp/src/pages/swipe/ui/SwipeStyle.ts
+++ Native/MyAndroidApp/src/pages/swipe/ui/SwipeStyle.ts
@@ -2,7 +2,48 @@ import { StyleSheet } from "react-native";
 import Colors from "../../../features/config/Colors";
 
 const SwipeStyle = StyleSheet.create({
+    pageContainer:
+    {
 
+    },
+    gameField:
+    {
+        backgroundColor: "#555",
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        // justifyContent: "space-between",
+    },
+    tile:
+    {
+        height: 80.0,
+        width: 80.0,
+        backgroundColor: "#666",
+        alignItems: "center",
+        justifyContent: "center",
+        marginBottom: 10.0,
+        marginRight: 7.0,
+        marginLeft: 10.0,
+        marginTop: 8.0,
+    },
+    textBlock:
+    {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: 20.0,
+    },
+    tileContainer:
+    {
+        height: 80.0,
+        width: 80.0,
+        alignItems: "center",
+        justifyContent: "center",
+        marginBottom: 10.0,
+        marginRight: 7.0,
+        marginLeft: 10.0,
+        marginTop: 8.0,
+    },
 });
 
 export default SwipeStyle;
